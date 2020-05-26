64007220c8518457de56ee4254e26271641cc554
B. Keyport <brendan.keyport@gmail.com>
2020-05-25 18:04:09 -0700

    change requested by author

--- node_modules/node-dht-sensor/build/Release/.deps/Release/node_dht_sensor.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/node_dht_sensor.node := rm -rf "Release/node_dht_sensor.node" && cp -af "Release/obj.target/node_dht_sensor.node" "Release/node_dht_sensor.node"
--- node_modules/node-dht-sensor/src/dht-sensor.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DHT_H
-#define _DHT_H
-
-int initialize();
-unsigned long long getTime();
-long readDHT(int type, int pin, float &temperature, float &humidity);
-
-#endif
